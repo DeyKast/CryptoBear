7caee2f8d6d44db30feea7564d9c646141c2d88a
Vladyslav Daniuk <109087019+DeyKast@users.noreply.github.com>
2023-12-05 02:29:24 +0200

    fix-background-and-scroll-lock

--- js/mobileMenuToggle.js
+++ js/mobileMenuToggle.js
@@ -11,6 +11,14 @@
       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
     mobileMenu.classList.toggle("is-open");
+
+    document.body.style.overflow = isMenuOpen ? "auto" : "hidden";
+  };
+
+  const preventScroll = (e) => {
+    if (mobileMenu.classList.contains("is-open")) {
+      e.preventDefault();
+    }
   };
 
   openMenuBtn.addEventListener("click", toggleMenu);
@@ -18,13 +26,14 @@
 
   navListItems.forEach((navListItem) => {
     navListItem.addEventListener("click", toggleMenu);
-    
   });
 
-  // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
     mobileMenu.classList.remove("is-open");
     openMenuBtn.setAttribute("aria-expanded", false);
+    document.body.style.overflow = "auto";
   });
+
+  document.addEventListener("wheel", preventScroll, { passive: false });
 })();
